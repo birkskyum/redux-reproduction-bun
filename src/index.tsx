401564fd1c7dc74abb5007c8a7814ea4b5599f4e
Birk Skyum <birk.skyum@pm.me>
2023-08-29 17:59:26 +0200

    Redux counter example

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import store from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
